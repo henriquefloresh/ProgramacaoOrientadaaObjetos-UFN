97cebfbee36091dca5f3097bc85776bc6ec7147a
Henrique Flores Hiemer <h.hiemer@ufn.edu.br>
2026-03-03 10:04:20 -0300

    Create Ex10.java

---
+++ Ex10.java
@@ -0,0 +1,25 @@
+package pkg;
+
+import java.util.Scanner;
+
+public class Ex10 {
+
+	public static void main(String[] args) {
+		Scanner teclado = new Scanner(System.in);
+		double n, somatorio = 0, media = 0;
+		int i = 0;
+		while(true) {
+			System.out.println("Digite um valor: ");
+			n = teclado.nextDouble();
+			somatorio+=n;
+			if(n<=0) {
+				break;
+			}
+			i++;
+		}
+			
+		media = somatorio / i;
+		System.out.println("Média: "+media);
+		}
+
+	}
