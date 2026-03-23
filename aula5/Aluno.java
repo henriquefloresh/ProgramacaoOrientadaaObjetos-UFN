78ccaaa4322dbdd9083528d88b92dbe658bbbb12
Henrique Flores Hiemer <h.hiemer@ufn.edu.br>
2026-03-23 16:51:39 -0300

    Create Aluno.java

---
+++ aula5/Aluno.java
@@ -0,0 +1,16 @@
+package pkg;
+//ex6
+public class Aluno {
+
+	public static void main(String[] args) {
+
+        // Objeto usando construtor 1
+        ClasseAluno a1 = new ClasseAluno("Henrique", "102025");
+
+        // Objeto usando construtor 2
+        ClasseAluno a2 = new ClasseAluno("03/12/2006");
+
+        // Objeto usando construtor 3
+        ClasseAluno a3 = new ClasseAluno("Wilson", "07/11/2006", 2025);
+    }
+}
