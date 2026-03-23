35e49bdda2a5f120dc125c766d265778a207fc65
Henrique Flores Hiemer <h.hiemer@ufn.edu.br>
2026-03-23 15:58:07 -0300

    Create Disc.java

---
+++ aula5/Disc.java
@@ -0,0 +1,39 @@
+package pkg;
+
+import java.util.Scanner;
+
+public class Disc {
+	public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        Disciplina d = new Disciplina();
+
+        // a) Conteúdo original
+        System.out.println(" Conteúdo ORIGINAL ");
+        System.out.println("Nome: " + d.nome);
+        System.out.println("Carga Horária: " + d.cargaHoraria);
+        System.out.println("Professor: " + d.professor);
+
+        // b) Entrada de dados
+        System.out.println("\n Digite os novos valores ");
+
+        System.out.print("Nome da disciplina: ");
+        d.nome = scanner.nextLine();
+
+        System.out.print("Carga horária: ");
+        d.cargaHoraria = scanner.nextInt();
+        scanner.nextLine();
+
+        System.out.print("Nome do professor: ");
+        d.professor = scanner.nextLine();
+
+        // c) Novo conteúdo
+        System.out.println("\n Novo Conteúdo ");
+        System.out.println("Nome: " + d.nome);
+        System.out.println("Carga Horária: " + d.cargaHoraria);
+        System.out.println("Professor: " + d.professor);
+
+        scanner.close();
+    }
+
+}
