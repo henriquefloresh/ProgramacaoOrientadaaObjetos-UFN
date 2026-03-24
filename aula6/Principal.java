c6539f47884b90da8b2277238967a27abcbc0196
Henrique Flores Hiemer <h.hiemer@ufn.edu.br>
2026-03-24 08:58:34 -0300

    Create Principal.java

---
+++ aula6/Principal.java
@@ -0,0 +1,30 @@
+public class Principal {
+    public static void main(String[] args) {
+        /*try { //exemplo 1
+            int[] numeros = {1, 2, 3};
+            System.out.println(numeros[3]);
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Erro indice fora do array \n"+e.getMessage());
+        } catch (Exception e) {
+            System.out.println("Exceção: "+e.toString());
+        }*/
+
+        //exemplo 2
+          /*  int a = 10;
+            int b = 0;
+        try {
+            System.out.println(a / b);
+        } catch (ArithmeticException e) {
+            System.out.println("Erro: " + e.getMessage());
+        }*/
+
+        //exemplo 3
+           /* String numero = "abc";
+        try {
+            int valor = Integer.parseInt(numero);
+        } catch (NumberFormatException e) {
+            System.out.println("Exceção: " + e.toString());
+        }*/
+
+    }
+}
