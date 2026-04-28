b5c8ee2f4d5d57e4d936f6ca7308d172154c4c5d
Henrique Flores Hiemer <h.hiemer@ufn.edu.br>
2026-04-28 10:33:54 -0300

    Create Desenho.java

---
+++ aula9/Desenho.java
@@ -0,0 +1,17 @@
+package pkg4;
+
+public class Desenho {
+	protected String nomeAutor;
+
+	public String getNomeAutor() {
+		return nomeAutor;
+	}
+
+	public void setNomeAutor(String nomeAutor) {
+		this.nomeAutor = nomeAutor;
+	}
+
+	public Desenho(String nomeAutor) {
+		super();
+		this.nomeAutor = nomeAutor;
+	}
