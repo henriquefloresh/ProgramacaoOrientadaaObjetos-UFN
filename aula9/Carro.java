006456424864bcb11753241370b2dd1dd25d4910
Henrique Flores Hiemer <h.hiemer@ufn.edu.br>
2026-04-28 08:35:25 -0300

    Create Carro.java

---
+++ aula9/Carro.java
@@ -0,0 +1,20 @@
+package pkg1;
+
+public class Carro {
+	protected String nome;
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public void exibeMsg(){
+		System.out.println("Estou dentro da classe carro");
+		System.out.println("O carro é: "+nome);
+
+	}
+
+}
