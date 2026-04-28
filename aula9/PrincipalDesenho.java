20cf92d2cf1f352dc3fcf9ba400e6489b19b4208
Henrique Flores Hiemer <h.hiemer@ufn.edu.br>
2026-04-28 10:35:52 -0300

    Create PrincipalDesenho.java

---
+++ aula9/PrincipalDesenho.java
@@ -0,0 +1,12 @@
+package pkg4;
+
+public class PrincipalDesenho {
+
+	public static void main(String[] args) {
+		Quadrado q = new Quadrado(100, 150, "Quadrado do Henrique", "Henrique Hiemer");
+		q.exibeDados();
+		
+		
+	}
+
+}
