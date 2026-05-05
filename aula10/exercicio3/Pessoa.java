2050d09a2e2b0ae055cb904e61b5618621106a0a
Henrique Flores Hiemer <h.hiemer@ufn.edu.br>
2026-05-05 10:26:12 -0300

    Create Pessoa.java

---
+++ aula10/exercicio3/Pessoa.java
@@ -0,0 +1,14 @@
+package pkg1;
+
+public class Pessoa {
+	
+	public void dizerOla() {
+		System.out.println("Ola! ");
+	}
+	
+	public void dizerOla(String nome) {
+		System.out.println("Ola "+nome+"!");
+		
+	}
+
+}
