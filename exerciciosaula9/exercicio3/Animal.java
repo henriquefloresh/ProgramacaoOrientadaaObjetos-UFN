c4a4a0e70870d7af37756024c323cd0ccf4ce92c
Henrique Flores Hiemer <h.hiemer@ufn.edu.br>
2026-05-05 09:52:09 -0300

    Add Animal class with tipo attribute and andar method

---
+++ exerciciosaula9/exercicio3/Animal.java
@@ -0,0 +1,11 @@
+class Animal {
+    String tipo;
+
+    public Animal(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public void andar() {
+        System.out.println("Todos andam, mas o modo é variado");
+    }
+}
