fbc5bf8306f842114aa3055a2db55b0a16d10d41
Henrique Flores Hiemer <h.hiemer@ufn.edu.br>
2026-06-08 17:36:06 -0300

    Create Carro.java

---
+++ aula15/Veiculo/Carro.java
@@ -0,0 +1,16 @@
+class Carro extends Veiculo {
+
+    public Carro(String marca, String modelo, int ano) {
+        super(marca, modelo, ano);
+    }
+
+    @Override
+    public void acelerar() {
+        System.out.println("O carro " + marca + " " + modelo + " está acelerando.");
+    }
+
+    @Override
+    public void frear() {
+        System.out.println("O carro " + marca + " " + modelo + " está freando.");
+    }
+}
