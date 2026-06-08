89f9c0c325990275e02782f91bf1dd3694bd9022
Henrique Flores Hiemer <h.hiemer@ufn.edu.br>
2026-06-08 17:36:38 -0300

    Create Moto.java

---
+++ aula15/Veiculo/Moto.java
@@ -0,0 +1,16 @@
+class Moto extends Veiculo {
+
+    public Moto(String marca, String modelo, int ano) {
+        super(marca, modelo, ano);
+    }
+
+    @Override
+    public void acelerar() {
+        System.out.println("A moto " + marca + " " + modelo + " está acelerando.");
+    }
+
+    @Override
+    public void frear() {
+        System.out.println("A moto " + marca + " " + modelo + " está freando.");
+    }
+}
