7b2b212ebad0871142b242739c5f03a587b3b3fc
Henrique Flores Hiemer <h.hiemer@ufn.edu.br>
2026-06-09 09:19:54 -0300

    Create abstract Conta class with saldo and limite

---
+++ aula16/conta/Conta.java
@@ -0,0 +1,13 @@
+public abstract class Conta {
+    protected double saldo;
+    protected double limite;
+    public abstract void depositar(double valor) throws SaldoInsuficienteException;;
+    public abstract void sacar(double valor) throws SaldoInsuficienteException;;
+    public Conta(double saldo, double limite) {
+        this.saldo = saldo;
+        this.limite = limite;
+    }
+    public double getSaldo() {
+        return saldo;
+    }
+}
