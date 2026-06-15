86a89539f623113a62f1fbc17c2d4630616c9e1e
Henrique Flores Hiemer <h.hiemer@ufn.edu.br>
2026-06-15 18:44:14 -0300

    Update MainPessoa.java

--- aula16/Pessoa/MainPessoa.java
+++ aula16/Pessoa/MainPessoa.java
@@ -1,17 +1,9 @@
 public class MainPessoa {
     public static void main(String[] args) {
 
-        PessoaFisica pf = new PessoaFisica(
-                "João",
-                "Santa Maria",
-                "123.456.789-00"
-        );
+        PessoaFisica pf = new PessoaFisica("Henrique", "Santa Maria", "03732645002");
 
-        PessoaJuridica pj = new PessoaJuridica(
-                "Empresa ABC",
-                "Porto Alegre",
-                "12.345.678/0001-99"
-        );
+        PessoaJuridica pj = new PessoaJuridica("Empresa ABC", "Porto Alegre", "12.345.678/0001-99");
 
         pf.efetuarCompra(500);
         pj.efetuarCompra(2000);
