1d931ab76113f356bc907288c3c4aeaf38078b94
Henrique Flores Hiemer <h.hiemer@ufn.edu.br>
2026-06-15 18:35:30 -0300

    Implement CD class that conforms to Produto interface

---
+++ aula16/EX_INTERFACE/CD.java
@@ -0,0 +1,27 @@
+public class CD implements Produto {
+
+    private String nome;
+    private double preco;
+    private String descricao;
+
+    public CD(String nome, double preco, String descricao) {
+        this.nome = nome;
+        this.preco = preco;
+        this.descricao = descricao;
+    }
+
+    @Override
+    public String getNome() {
+        return nome;
+    }
+
+    @Override
+    public double getPreco() {
+        return preco;
+    }
+
+    @Override
+    public String getDescricao() {
+        return descricao;
+    }
+}
