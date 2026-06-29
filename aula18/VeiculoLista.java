4e7249f5f7590e17d8e41f623a8bd926846a3930
Henrique Flores Hiemer <h.hiemer@ufn.edu.br>
2026-06-29 20:04:58 -0300

    Update VeiculoLista.java

--- aula18/VeiculoLista.java
+++ aula18/VeiculoLista.java
@@ -1,13 +1,16 @@
 import java.io.*;
 import java.util.ArrayList;
 
+//Classe responsavel por salvar e ler os veiculos do arquivo
 public class VeiculosLista {
 
     private static final String NOME_ARQUIVO = "veiculos.dat";
 
+    //Salva a lista de veículos no arquivo
     public static void gravar(ArrayList<Veiculo> veiculos) {
         try {
             ObjectOutputStream saida = new ObjectOutputStream(new FileOutputStream(NOME_ARQUIVO));
+            //Grava toda a lista
             saida.writeObject(veiculos);
             saida.close();
         } catch (IOException erro) {
@@ -15,6 +18,7 @@ public class VeiculosLista {
         }
     }
 
+    //Le os veiculos do arquivos
     public static ArrayList<Veiculo> ler() {
         try {
             ObjectInputStream entrada = new ObjectInputStream(new FileInputStream(NOME_ARQUIVO));
@@ -22,6 +26,7 @@ public class VeiculosLista {
             entrada.close();
             return veiculos;
         } catch (Exception erro) {
+            //Usa return se o arquivo não existir, então retorna uma lista vazia
             return new ArrayList<Veiculo>();
         }
     }
