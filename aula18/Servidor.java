1c44e7f94c07f4e8c88f313644ae5938e9644c8e
Henrique Flores Hiemer <h.hiemer@ufn.edu.br>
2026-06-29 20:15:59 -0300

    Update Servidor.java

--- aula18/Servidor.java
+++ aula18/Servidor.java
@@ -2,28 +2,36 @@ import java.io.*;
 import java.net.*;
 import java.util.ArrayList;
 
+//Servidor responsável por receber os veículos do cliente
 public class Servidor {
 
     public static void main(String[] args) {
 
+        //Porta que sera utilizada para se comunicar
         int porta = 12345;
         ArrayList<Veiculo> lista = VeiculoLista.carregar();
 
         try {
+            //Cria o servidor e fica aguardando conexões
             ServerSocket servidor = new ServerSocket(porta);
             System.out.println("Servidor iniciado na porta " + porta);
 
             while (true) {
+
+                //Aguarda um cliente se conectar
                 Socket cliente = servidor.accept();
                 System.out.println("Cliente conectado.");
 
+                //Cria os canais de entrada e saída de dados
                 ObjectInputStream entrada = new ObjectInputStream(cliente.getInputStream());
                 ObjectOutputStream saida = new ObjectOutputStream(cliente.getOutputStream());
 
+                //Recebe o objeto veiculo enviado pelo cliente
                 Veiculo veiculo = (Veiculo) entrada.readObject();
 
                 boolean existe = false;
 
+                //esta verificando se ja existe um veiculo com a mesma placa
                 for (Veiculo v : lista) {
                     if (v.getPlaca().equalsIgnoreCase(veiculo.getPlaca())) {
                         existe = true;
@@ -35,6 +43,7 @@ public class Servidor {
                     saida.writeObject("Cadastro não efetuado. Placa já existe.");
                 } else {
                     lista.add(veiculo);
+                    //Atualiza o arquivo com a nova lista
                     VeiculoLista.salvar(lista);
                     saida.writeObject("Cadastro efetuado com sucesso.");
                 }
@@ -44,6 +53,7 @@ public class Servidor {
                 cliente.close();
             }
 
+        //caso ocorra erro, exibe essa mensagem
         } catch (Exception e) {
             System.out.println("Erro no servidor: " + e.getMessage());
         }
